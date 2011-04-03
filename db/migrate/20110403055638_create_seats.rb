42170d9ddcdd7932da2f9853205317525e96bf8d
Rondle <programmerRonald@yahoo.com>
2011-04-03 12:19:34 -0700

    Edited db/migrate/20110403055638_create_seats.rb via GitHub

--- db/migrate/20110403055638_create_seats.rb
+++ db/migrate/20110403055638_create_seats.rb
@@ -1,12 +1,2 @@
 class CreateSeats < ActiveRecord::Migration
-  def self.up
-    create_table :seats do |t|
-
-      t.timestamps
-    end
-  end
-
-  def self.down
-    drop_table :seats
-  end
 end
